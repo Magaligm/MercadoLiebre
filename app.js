9519c9b767ab63f9913eb6c40b4fe009539a58eb
Magali Garcia Muñoz <magalinicole.gm@gmail.com>
2022-03-22 21:23:03 -0300

    proyecto mercado liebre

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static("public"));
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/home.html");
+});
+
+app.get("/register", (req, res) => {
+  res.sendFile(__dirname + "/views/register.html");
+});
+
+app.get("/login", (req, res) => {
+  res.sendFile(__dirname + "/views/login.html");
+});
+
+app.listen(5353, () => {
+  console.log("El servidor esta corriendo en el puerto 5353");
+});
